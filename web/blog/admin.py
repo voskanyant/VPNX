0a67a88551b903cbc76249ccc357a8ade41234c6
Tigran Voskanyan <C:\Users\tigra\AppData\Roaming\The Bat!>
2026-04-04 21:32:42 +0300

    fix(backoffice): refresh document canvas ui and bust asset cache

--- web/blog/admin.py
+++ web/blog/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 from .models import Category, Page, Post, PostType, SiteText
 
-BLOCK_EDITOR_ASSET_VERSION = "20260404-flatsome-ux-v8"
+BLOCK_EDITOR_ASSET_VERSION = "20260404-document-ux-v9"
 
 
 class RichTextAdminForm(forms.ModelForm):
